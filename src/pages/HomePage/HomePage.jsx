a92326253823c5b56841e7e7a655142eeaa131e5
maryxm9 <maryxm9@hotmail.com>
2024-06-07 00:19:05 -0700

    make homepage show the best rated drinks

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,28 @@
-import "./HomePage.scss"; 
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import DrinksData from '../../DrinksData.json';
 
 function HomePage() {
+  // Find the highest-rated bubble tea drink
+  let highestRatedDrink = null;
+  let shopName = null;
+
+  for (const restaurant of DrinksData.restaurants) {
+    for (const drink of restaurant.drinks) {
+      if (!highestRatedDrink || drink.rating > highestRatedDrink.rating) {
+        highestRatedDrink = drink;
+        shopName = restaurant.name;
+      }
+    }
+  }
+
   return (
-   <></>
+    <div>
+      <h1>Welcome to the Bubble Tea Shop!</h1>
+      <h2>Highest Rated Bubble Tea:</h2>
+      <p>Drink: {highestRatedDrink.name}</p>
+      <p>Shop Name: {shopName}</p>
+    </div>
   );
 }
 
