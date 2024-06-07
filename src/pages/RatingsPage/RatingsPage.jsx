54efc516f2b477d5b24a9f4d483d99ec72456dda
maryxm9 <maryxm9@hotmail.com>
2024-06-07 00:37:04 -0700

    modify ratings page to display outside of restaurant constraint

--- src/pages/RatingsPage/RatingsPage.jsx
+++ src/pages/RatingsPage/RatingsPage.jsx
@@ -4,6 +4,10 @@ import restaurantsData from '../../DrinksData.json';
 import './RatingsPage.scss';
 
 function RatingsPage() {
+  const allDrinks = restaurantsData.restaurants.reduce((acc, curr) => {
+    return [...acc, ...curr.drinks];
+  }, []);
+
   // Highest to lowest rating
   const compareRatings = (a, b) => b.rating - a.rating;
 
@@ -11,36 +15,30 @@ function RatingsPage() {
     <div>
       <h1>Bubble Tea Ratings</h1>
       <div className="bubble-tea-list">
-        {restaurantsData.restaurants.map((restaurant) => (
-          <div className="restaurant" key={restaurant.id}>
-            <h2>{restaurant.name}</h2>
-            <ul className="drink-container">
-              {restaurant.drinks.sort(compareRatings).map((drink) => (
-                <li className="drink" key={drink.id}>
-                  <Link to={`/business/${restaurant.id}`}>
-                    <div className="drink-img-container">
-                      <img
-                        className="drink-img"
-                        src={`${process.env.PUBLIC_URL}/assets/images/${drink.image}`}
-                        alt={drink.name}
-                      />
-                    </div>
-                    <div className="drink-details">
-                      <h3>{drink.name}</h3>
-                      <p>Price: ${drink.price}</p>
-                      <p>Store: {restaurant.name}</p>
-                      <p>Rating: {drink.rating}</p>
-                    </div>
-                  </Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-        ))}
+        <ul className="drink-container">
+          {allDrinks.sort(compareRatings).map((drink) => (
+            <li className="drink" key={drink.id}>
+              <Link to={`/business/${drink.id}`}>
+                <div className="drink-img-container">
+                  <img
+                    className="drink-img"
+                    src={`${process.env.PUBLIC_URL}/assets/images/${drink.image}`}
+                    alt={drink.name}
+                  />
+                </div>
+                <div className="drink-details">
+                  <h3>{drink.name}</h3>
+                  <p>Price: ${drink.price}</p>
+                  <p>Store: {drink.store}</p>
+                  <p>Rating: {drink.rating}</p>
+                </div>
+              </Link>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
 }
 
 export default RatingsPage;
-
