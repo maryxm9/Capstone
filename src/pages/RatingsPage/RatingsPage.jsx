fd8efebd096050576c32932a12f43e28fc028fe2
maryxm9 <maryxm9@hotmail.com>
2024-06-08 16:29:20 -0700

    add ranking #

--- src/pages/RatingsPage/RatingsPage.jsx
+++ src/pages/RatingsPage/RatingsPage.jsx
@@ -22,32 +22,34 @@ function RatingsPage() {
       <h1>Bubble Tea Ratings</h1>
       <div className="bubble-tea-list">
         <ul className="drink-container">
-          {allDrinks.sort(compareRatings).map((drink) => (
+          {allDrinks.sort(compareRatings).map((drink, index) => (
             <li className="drink" key={drink.id}>
-              
-                <div className="drink-row">
-                  <div className="drink-img-container">
-                    
-                    <img
-                      className="drink-img"
-                      src={`${process.env.PUBLIC_URL}/assets/images/${drink.image}`}
-                      alt={drink.name}
-                    />
-                  </div>
-                  <div className="drink-details">
-                    <div className="drink-details-left">
-                      <Link to={`/business/${drink.storeId}`} className="rating-links">
+              <div className="drink-row">
+                <h3>#{index + 1}</h3>
+                <div className="drink-img-container">
+                  <img
+                    className="drink-img"
+                    src={`${process.env.PUBLIC_URL}/assets/images/${drink.image}`}
+                    alt={drink.name}
+                  />
+                </div>
+                <div className="drink-details">
+                  <div className="drink-details-left">
+                    <Link
+                      to={`/business/${drink.storeId}`}
+                      className="rating-links"
+                    >
                       <h3>{drink.name}</h3>
                       <p>${drink.price}</p>
-                      <p>{drink.storeName}</p></Link>
-                    </div>
-                    <div className="star-ratings">
-                      <StarRating rating={drink.rating} />
-                      <p>{drink.rating.toFixed(1)}</p>
-                    </div>
+                      <p>{drink.storeName}</p>
+                    </Link>
+                  </div>
+                  <div className="star-ratings">
+                    <StarRating rating={drink.rating} />
+                    <p>{drink.rating.toFixed(1)}</p>
                   </div>
                 </div>
-              
+              </div>
             </li>
           ))}
         </ul>
